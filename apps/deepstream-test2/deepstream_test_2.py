7ac7c57e65029302c15a6c0e7ea1a221b91192e1
jaweiler5454 <jaweiler5454@gmail.com>
2021-01-13 15:22:00 -0500

    testing refactored primary inference code

--- apps/deepstream-test2/deepstream_test_2.py
+++ apps/deepstream-test2/deepstream_test_2.py
@@ -122,13 +122,13 @@ def create_source_bin(index,uri):
 def osd_sink_pad_buffer_probe(pad,info,u_data):
     frame_number=0
     #Intiallizing object counter with 0.
-    obj_counter = {
-        PGIE_CLASS_ID_VEHICLE:0,
-        PGIE_CLASS_ID_PERSON:0,
-        PGIE_CLASS_ID_BICYCLE:0,
-        PGIE_CLASS_ID_ROADSIGN:0
-    }
-    num_rects=0
+#     obj_counter = {
+#         PGIE_CLASS_ID_VEHICLE:0,
+#         PGIE_CLASS_ID_PERSON:0,
+#         PGIE_CLASS_ID_BICYCLE:0,
+#         PGIE_CLASS_ID_ROADSIGN:0
+#     }
+#     num_rects=0
     gst_buffer = info.get_buffer()
     if not gst_buffer:
         print("Unable to get GstBuffer ")
@@ -138,9 +138,6 @@ def osd_sink_pad_buffer_probe(pad,info,u_data):
     # Note that pyds.gst_buffer_get_nvds_batch_meta() expects the
     # C address of gst_buffer as input, which is obtained with hash(gst_buffer)
     batch_meta = pyds.gst_buffer_get_nvds_batch_meta(hash(gst_buffer))
-    classifier_meta = pyds.NvDsClassifierMeta
-    print("NUM_LABELS" + str(classifier_meta.cast(batch_meta.classifier_meta_pool).num_labels))
-#    print(pyds.NvDsLabelInfo.cast().result_label)
     l_frame = batch_meta.frame_meta_list
     while l_frame is not None:
         try:
@@ -154,76 +151,16 @@ def osd_sink_pad_buffer_probe(pad,info,u_data):
         except StopIteration:
             break
 
-        frame_number=frame_meta.frame_num
-        num_rects = frame_meta.num_obj_meta
         frame_user_meta_list = frame_meta.frame_user_meta_list
         obj_user_meta_list = obj_meta.obj_user_meta_list
         if obj_user_meta_list is not None:
-            print("Obj Meta", obj_user_meta_list)
+            user_meta = pyds.NvDsUserMeta.cast(obj_user_meta_list.data)
+            tensor_meta = pyds.NvDsInferTensorMeta.cast(user_meta.user_meta_data)
+            print(tensor_meta)
         if frame_user_meta_list is not None:
-            print("Frame Meta", frame_user_meta_list)
-        count = 0
-#         for el in obj_user_meta_list:
-#             print(count + ":" + pyds.NvDsUserMeta.cast(el).user_meta_data)
-#        l_obj=frame_meta.obj_meta_list
-        print('label info',pyds.NvDsLabelInfo.cast(batch_meta.label_info_meta_pool).label_id)
-        if 1:
-#        while l_obj is not None:
-            try:
-                
-                label_id = pyds.NvDsLabelInfo.cast(batch_meta.label_info_meta_pool).label_id
-                # Casting l_obj.data to pyds.NvDsObjectMeta
-#                print('frame meta',frame_meta)
-                n_frame=pyds.get_nvds_buf_surface(hash(gst_buffer),frame_meta.batch_id)
-                    #convert python array into numy array format.
-                frame_image=np.array(n_frame,copy=True,order='C')
-                    #covert the array into cv2 default color format
-                frame_image=cv2.cvtColor(frame_image,cv2.COLOR_RGBA2BGRA)
-                # print(f'{folder_name}/0/frame_{frame_number}.jpg')
-                if label_id == 0:
-                    cv2.imwrite(f'{folder_name}/0/frame_{frame_number}.jpg',frame_image)
-                elif label_id == 127:
-                    cv2.imwrite(f'{folder_name}/127/frame_{frame_number}.jpg',frame_image)
-                else:
-                    cv2.imwrite(f'{folder_name}/other/frame_{frame_number}_{label_id}.jpg',frame_image)
-#cv2.imwrite("/opt/nvidia/deepstream/deepstream-5.0/mount/images1"+"/stream_0"+"/frame_"+str(frame_number)+".jpg",frame_image)
-            except StopIteration:
-                break
-#            obj_counter[obj_meta.class_id] += 1
-#            try: 
-#                l_obj=l_obj.next
-#            except StopIteration:
-#                break
-        # Acquiring a display meta object. The memory ownership remains in
-        # the C code so downstream plugins can still access it. Otherwise
-        # the garbage collector will claim it when this probe function exits.
-        display_meta=pyds.nvds_acquire_display_meta_from_pool(batch_meta)
-        display_meta.num_labels = 1
-        py_nvosd_text_params = display_meta.text_params[0]
-        # Setting display text to be shown on screen
-        # Note that the pyds module allocates a buffer for the string, and the
-        # memory will not be claimed by the garbage collector.
-        # Reading the display_text field here will return the C address of the
-        # allocated string. Use pyds.get_string() to get the string content.
-        py_nvosd_text_params.display_text = "Frame Number={} Number of Objects={} Vehicle_count={} Person_count={}".format(frame_number, num_rects, obj_counter[PGIE_CLASS_ID_VEHICLE], obj_counter[PGIE_CLASS_ID_PERSON])
-        fps_streams["stream{0}".format(frame_meta.pad_index)].get_fps()
-        # Now set the offsets where the string should appear
-        py_nvosd_text_params.x_offset = 10
-        py_nvosd_text_params.y_offset = 12
-
-        # Font , font-color and font-size
-        py_nvosd_text_params.font_params.font_name = "Serif"
-        py_nvosd_text_params.font_params.font_size = 10
-        # set(red, green, blue, alpha); set to White
-        py_nvosd_text_params.font_params.font_color.set(1.0, 1.0, 1.0, 1.0)
-
-        # Text background color
-        py_nvosd_text_params.set_bg_clr = 1
-        # set(red, green, blue, alpha); set to Black
-        py_nvosd_text_params.text_bg_clr.set(0.0, 0.0, 0.0, 1.0)
-        # Using pyds.get_string() to get display_text as string
-        print(pyds.get_string(py_nvosd_text_params.display_text))
-        pyds.nvds_add_display_meta_to_frame(frame_meta, display_meta)
+            user_meta = pyds.NvDsUserMeta.cast(frame_user_meta_list.data)
+            tensor_meta = pyds.NvDsInferTensorMeta.cast(user_meta.user_meta_data)
+            print(tensor_meta)
         try:
             l_frame=l_frame.next
         except StopIteration:
@@ -248,10 +185,6 @@ def main(args):
 
     os.mkdir(folder_name)
     print("Frames will be saved in ",folder_name)
-    os.mkdir(f'{folder_name}/0')
-    os.mkdir(f'{folder_name}/127')
-    os.mkdir(f'{folder_name}/other')
-
 
     # Standard GStreamer initialization
     GObject.threads_init()
@@ -328,19 +261,6 @@ def main(args):
     if not tracker:
         sys.stderr.write(" Unable to create tracker \n")
 
-    sgie1 = Gst.ElementFactory.make("nvinfer", "secondary1-nvinference-engine")
-    if not sgie1:
-        sys.stderr.write(" Unable to make sgie1 \n")
-
-
-    sgie2 = Gst.ElementFactory.make("nvinfer", "secondary2-nvinference-engine")
-    if not sgie1:
-        sys.stderr.write(" Unable to make sgie2 \n")
-
-    sgie3 = Gst.ElementFactory.make("nvinfer", "secondary3-nvinference-engine")
-    if not sgie3:
-        sys.stderr.write(" Unable to make sgie3 \n")
-
     nvvidconv = Gst.ElementFactory.make("nvvideoconvert", "convertor")
     if not nvvidconv:
         sys.stderr.write(" Unable to create nvvidconv \n")
@@ -368,80 +288,26 @@ def main(args):
     streammux.set_property('batched-push-timeout', 4000000)
 
     #Set properties of pgie and sgie
-    pgie.set_property('config-file-path', "dstest2_pgie_config.txt")
-    sgie1.set_property('config-file-path', "dstest2_sgie1_config.txt")
-    sgie2.set_property('config-file-path', "dstest2_sgie2_config.txt")
-    sgie3.set_property('config-file-path', "dstest2_sgie3_config.txt")
+    pgie.set_property('config-file-path', "dstest2_sgie2_config.txt")
 
-    #Set properties of tracker
-    config = configparser.ConfigParser()
-    config.read('dstest2_tracker_config.txt')
-    config.sections()
-
-    for key in config['tracker']:
-        if key == 'tracker-width' :
-            tracker_width = config.getint('tracker', key)
-            tracker.set_property('tracker-width', tracker_width)
-        if key == 'tracker-height' :
-            tracker_height = config.getint('tracker', key)
-            tracker.set_property('tracker-height', tracker_height)
-        if key == 'gpu-id' :
-            tracker_gpu_id = config.getint('tracker', key)
-            tracker.set_property('gpu_id', tracker_gpu_id)
-        if key == 'll-lib-file' :
-            tracker_ll_lib_file = config.get('tracker', key)
-            tracker.set_property('ll-lib-file', tracker_ll_lib_file)
-        if key == 'll-config-file' :
-            tracker_ll_config_file = config.get('tracker', key)
-            tracker.set_property('ll-config-file', tracker_ll_config_file)
-        if key == 'enable-batch-process' :
-            tracker_enable_batch_process = config.getint('tracker', key)
-            tracker.set_property('enable_batch_process', tracker_enable_batch_process)
 
     print("Adding elements to Pipeline \n")
-    # pipeline.add(source)
-    # pipeline.add(h264parser)
-    # pipeline.add(decoder)
-    # pipeline.add(streammux)
-#    pipeline.add(pgie)
-#    pipeline.add(tracker)
-#    pipeline.add(sgie1)
-    pipeline.add(sgie2)
-#    pipeline.add(sgie3)
+    pipeline.add(pgie)
+
     pipeline.add(nvvidconv)
     pipeline.add(nvosd)
     pipeline.add(sink)
-#    if is_aarch64():
-#       pipeline.add(transform)
-
+    
     # we link the elements together
     # file-source -> h264-parser -> nvh264-decoder ->
     # nvinfer -> nvvidconv -> nvosd -> video-renderer
     print("Linking elements in the Pipeline \n")
-    # source.link(h264parser)
-    # h264parser.link(decoder)
+    
+    streammux.link(pgie)
 
-    # sinkpad = streammux.get_request_pad("sink_0")
-    # if not sinkpad:
-    #     sys.stderr.write(" Unable to get the sink pad of streammux \n")
-    # srcpad = decoder.get_static_pad("src")
-    # if not srcpad:
-    #     sys.stderr.write(" Unable to get source pad of decoder \n")
-    # srcpad.link(sinkpad)
-#    streammux.link(pgie)
-    streammux.link(sgie2)
-#    pgie.link(tracker)
-#    tracker.link(sgie1)
-#    sgie1.link(sgie2)
-#    sgie2.link(sgie3)
-    sgie2.link(nvvidconv)
+    pgie.link(nvvidconv)
     nvvidconv.link(nvosd)
-#    if is_aarch64():
-#        nvosd.link(transform)
-#        transform.link(sink)
-#    else:
     nvosd.link(sink)
-
 
     # create and event loop and feed gstreamer bus mesages to it
     loop = GObject.MainLoop()
@@ -464,9 +330,9 @@ def main(args):
     # start play back and listed to events
     pipeline.set_state(Gst.State.PLAYING)
     try:
-      loop.run()
+        loop.run()
     except:
-      pass
+        pass
 
     # cleanup
     pipeline.set_state(Gst.State.NULL)
